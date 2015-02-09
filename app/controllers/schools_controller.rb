2a739bf3cf778714dace37265db57f9b0fceed18
Clark Bremer <cbremer@ackmanndickenson.com>
2015-02-09 10:01:30 -0600

    Fixup address not found
    Attempt to solve N+1 problem

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -3,8 +3,8 @@ class SchoolsController < ApplicationController
   def index
     puts "@@@@ index:"
     @masp = Geokit::Geocoders::GoogleGeocoder.geocode '2500 Central Ave NE, Minneapolis, MN'
-    # @schools = School.includes(:rates).where(rates: {group: "All Students"}).all
-    @schools = School.all
+    @schools = School.includes(:rates).where(rates: {group: "All Students"})
+    #@schools = School.first(100)
     @schools = @schools.sort_by {|s| s.distance_to(@masp)}
   end
 
